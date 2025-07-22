2c695fb9e3db23a65265e2bc7546893bc645bf0d
ganapaiah <ganapaiahbandaru@gmail.com>
2025-07-22 23:41:45 +0530

    Added solution for Move Zeroes using two-pointer approach

---
+++ Array-Questions/moveZeroes/moveZeroes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int n=nums.size();
+        int i=0,j=1;
+        while(j<n){
+            if(nums[i]==0){
+                if(nums[j] !=0){
+                    swap(nums[i],nums[j]);
+                    i++;
+                    j++;
+
+                }
+                else{
+                    j++;
+                }
+                
+            }
+            else{
+                i++;
+                j++;
+            }
+            
+        }
+    }
+};
