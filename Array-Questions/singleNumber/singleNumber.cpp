25765b24125b2ea2cf939c9255aaf4da2a55e371
ganapaiah <ganapaiahbandaru@gmail.com>
2025-07-22 23:17:05 +0530

    Added solution for Single Number problem

---
+++ Array-Questions/singleNumber/singleNumber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        map<int,int>m;
+        for(int i=0;i<n;i++){
+            m[nums[i]]++;
+        }
+        int ans=-1;
+        for(auto res:m){
+            if(res.second==1){
+                ans=res.first;
+                break;
+            }
+        }
+        return ans;
+    }
+};
