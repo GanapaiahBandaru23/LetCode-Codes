81fb316c828c996bf4ec92503d63ec31e6a16ae2
ganapaiah <ganapaiahbandaru@gmail.com>
2025-07-22 23:52:09 +0530

    Added solution for Intersection of Two Arrays II using unordered_map

---
+++ Array-Questions/interSectionTwoArr2/interSectionTwoArr.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>v;
+        unordered_map<int,int>m;
+        for(int i=0;i<nums2.size();i++){
+            m[nums2[i]]++;
+        }
+        for(int i=0;i<nums1.size();i++){
+            if(m.find(nums1[i]) != m.end()){
+                if(m[nums1[i]]>0){
+                    v.push_back(nums1[i]);
+                    m[nums1[i]]--;
+                }
+            }
+
+        }
+        return v;
+    }
+};
