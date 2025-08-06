780d752a003712f0e69011557d82bb2b67bbbee1
ganapaiah <ganapaiahbandaru@gmail.com>
2025-08-06 17:52:52 +0530

    Add solution for Reshape the Matrix (LeetCode 566)

---
+++ Array-Questions/ReshapeTheMatrix/matrix_reshape.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<vector<int>>v(r,vector<int>(c));
+        if (m*n != r*c){
+            return mat;
+        }
+        for(int i=0;i<m*n;i++){
+            v[i/c][i%c]=mat[i/n][i%n];
+        }
+        return v;
+    }
+    
+};
