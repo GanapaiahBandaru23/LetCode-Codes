d80f6a502728e2ec0d2a0d7c82e195cedd48467c
ganapaiah <ganapaiahbandaru@gmail.com>
2025-08-12 21:26:32 +0530

    Implement minSubArrayLen function using sliding window approach

---
+++ Array-Questions/MinSubArrayLen/min_subarray_len.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n=nums.size();
+        int i=0,j=0,sum=0;
+        int min_len=INT_MAX;
+        while(j<n){
+            sum+=nums[j];
+            while(sum>=target){
+                
+                min_len=min(min_len,j-i+1);
+                
+                sum=sum-nums[i];
+                i++;
+            }
+            j++;
+
+        }
+        return (min_len == INT_MAX) ? 0 : min_len;
+      
+    }
+
+};
